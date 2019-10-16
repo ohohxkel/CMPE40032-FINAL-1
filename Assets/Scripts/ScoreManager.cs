51353cd5b251b2b06dfea56763c72eafcdf70ef2
ohohxkel <mikeecantara@gmail.com>
2019-10-16 11:26:36 +0800

    Added High Score Display

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,12 +6,13 @@ public class ScoreManager : MonoBehaviour
 {
     public static int score;        // The player's score.
 
-    Text text;                      // Reference to the Text component.
+    Text text, highScoreText;                   // Reference to the Text component.
 
     void Awake()
     {
         // Set up the reference.
         text = GetComponent<Text>();
+        highScoreText = GameObject.Find("HighScoreText").GetComponent<Text>();
 
         // Reset the score.
         score = 0;
@@ -22,5 +23,12 @@ public class ScoreManager : MonoBehaviour
     {
         // Set the displayed text to be the word "Score" followed by the score value.
         text.text = score.ToString();
+
+        if(PlayerPrefs.GetInt("HighScore")<= score)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+
+        highScoreText.text = PlayerPrefs.GetInt("HighScore").ToString();
     }
 }
---
+++ Assets/Scripts/Try.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Try : MonoBehaviour
+{
+
+    public GameObject Asteroid;
+
+    private float min_X = -7.37f;
+    private float max_X = 7.37f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    StartCoroutine(StartSpawning());
+    }
+
+
+    IEnumerator StartSpawning()
+    {
+    yield return new WaitForSeconds(Random.Range(1, 2));
+
+    GameObject k = Instantiate(Asteroid);
+
+    float x = Random.Range(min_X, max_X);
+
+    k.transform.position = new Vector2(x, 7.25f);
+
+    StartCoroutine(StartSpawning());
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
